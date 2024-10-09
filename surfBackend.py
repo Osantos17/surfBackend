94151ae51e32c133bcac8120e3c8b071223a63e1
Orlando Santos <Santos.Orlando17@gmail.com>
2024-10-09 01:27:28 -0700

    add api and gitnore

---
+++ surfBackend.py
@@ -0,0 +1,32 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv('config.env')
+
+def fetch_surf(lat, lng):
+
+    api_key = os.getenv('API_KEY')
+    
+    if not api_key:
+        print('Error: API Disconnected')
+        return
+        
+        
+    base_url = "http://api.worldweatheronline.com/premium/v1/marine.ashx"
+    params = {
+        'key': api_key,
+        'format': 'json',
+        'q': f'{lat},{lng}',  # Dynamic Latitude,Longitude #HMB '37.488897,-122.466919'
+    }
+
+    # GET request
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+    # Print the JSON response data
+        print(response.json())  # You can change this to save or filter the data
+    else:
+        print(f"Error: {response.status_code}")
+        
+fetch_surf(37.488897, -122.466919)
